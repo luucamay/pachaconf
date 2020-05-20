ef1ee1c8671ffeae1afd4510a071d0056878a9a9
luucamay <lupemaydana@gmail.com>
2020-05-19 23:12:56 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,67 @@
+import validator from './validator.js';
+
+// console.log(validator.isValid('4137894711755904'));
+let total = 0.00;
+
+const homeView = document.getElementById("homeView");
+const checkoutView = document.getElementById("checkoutView");
+const thanksView = document.getElementById("thanksView");
+const buyTicketsBtn = document.getElementById("buyTickets");
+const placeOrderBtn = document.getElementById("placeOrder");
+// Variables from buyView
+const buyView = document.getElementById("buyView");
+const checkoutBtn = document.getElementById("checkout");
+const generalTicketQuantity = document.getElementById("ticket-quantity-1");
+const partyTicketQuantity = document.getElementById("ticket-quantity-2");
+const agreeTermsConditions = document.getElementById("agreeTermsConditions");
+const totalBuy = document.getElementById("totalBuy")
+const totalCheckout = document.getElementById("totalCheckout")
+// Variables from checkoutView
+const firstName = document.getElementById("first-name");
+const email = document.getElementById("email");
+// Variables from thanksView
+const thanksName = document.getElementById("thanks-name");
+const thanksEmail = document.getElementById("thanks-email");
+const transformedCard = document.getElementById("transformed-card");
+
+const buyTickets = function () {
+    homeView.classList.add("hide");
+    buyView.classList.remove("hide");
+}
+
+const checkout = function (evt) {
+    evt.preventDefault();
+    if (total > 0 && agreeTermsConditions.checked) {
+        buyView.classList.add("hide");
+        checkoutView.classList.remove("hide");
+        totalCheckout.innerHTML = '$ ' + total.toFixed(2);
+    }
+}
+
+const placeOrder = function (evt) {
+    evt.preventDefault();
+    // when card number is valid
+    const cardNumber = document.getElementById("card-number").value;
+    if (validator.isValid(cardNumber)) {
+        checkoutView.classList.add("hide");
+        thanksView.classList.remove("hide");
+        thanksName.textContent = firstName.value;
+        thanksEmail.textContent = email.value;
+        transformedCard.textContent = validator.maskify(cardNumber);
+    } else {
+        alert("card number not valid, try again");
+    }
+}
+
+const updateTotalPrice = function () {
+    total = 0.00
+    total += generalTicketQuantity.value * 500;
+    total += partyTicketQuantity.value * 100;
+    totalBuy.innerHTML = '$ ' + total.toFixed(2);
+}
+
+buyTicketsBtn.addEventListener("click", buyTickets);
+checkoutBtn.addEventListener("click", checkout);
+placeOrderBtn.addEventListener("click", placeOrder);
+generalTicketQuantity.addEventListener("change", updateTotalPrice);
+partyTicketQuantity.addEventListener("change", updateTotalPrice);
---
+++ validator.js
@@ -0,0 +1,28 @@
+const validator = {
+  isValid: (creditCardNumber) => {
+    console.log(creditCardNumber)
+    const n = creditCardNumber.length;
+    let digitsSum = 0
+    for (let i = 0; i < n; i++) {
+      let currDigit = parseInt(creditCardNumber[i]);
+      if (i % 2 === 0) {
+        currDigit *= 2;
+        if (currDigit > 9)
+          currDigit -= 9;
+      }
+      digitsSum += currDigit;
+    }
+    if (digitsSum % 10 === 0)
+      return true;
+    return false;
+  },
+
+  maskify: (creditCardNumber) => {
+    const n = creditCardNumber.length;
+    if (n < 4)
+      return creditCardNumber;
+    return '🐱'.repeat(n - 4) + creditCardNumber.slice(-4);
+  }
+};
+
+export default validator;
